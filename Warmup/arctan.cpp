d410359d22df7c0047bb2f8ec3588a575ab565fa
XukunXiang <xxk08300190024@gmail.com>
2016-01-15 10:32:59 -0500

    set up the files

---
+++ Warmup/arctan.cpp
@@ -0,0 +1,6 @@
+//Program to compute first derivative of arctan(x) at $x = \sqrt(2)$ with two formulae
+using namespace std;
+
+#include <iostream>
+#include <cmath>
+#define TRUNCATION 
---
+++ Warmup/fortran/arctan.f90
@@ -0,0 +1,8 @@
+program arctan
+! Program to compute the first derivative of arctan(x) at x=sqrt(2)
+
+IMPLICIT NONE
+
+
+
+end program arctan
